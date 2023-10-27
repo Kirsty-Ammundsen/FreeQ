d161d9bc1ca81e9e7b6895a78ae8cb3f3880f961
Pathik Modi <modipathik95@gmail.com>
2023-10-27 13:36:50 +1300

    add tester for business page

--- server/server.js
+++ server/server.js
@@ -39,17 +39,25 @@ server.get('/', async (req, res) => {
 
 server.get('/business/:id', async (req, res) => {
   const viewData = {
-    businesses: [
-      { name: 'BarberShop', logo: 'barber.png' },
-      { name: 'Cute Doctors', logo: 'hospital.png' },
-      { name: 'Vampire Phlebs', logo: 'blood-test.png' },
+    tickets: [
+      {
+        name: 'BarberShop',
+        logo: 'barber.png',
+        email: 'ssss@ss.com',
+        avg_appointment_time_mins: 15,
+        queue: 5,
+      },
     ],
   }
+
+  // tickets.join('businesses')
+
   try {
     const id = req.params.id
     console.log(id)
     const business = await db.getBusiness(id)
-    res.send(`here's the business details ${id}`)
+    console.log(viewData)
+    res.render('business', viewData)
   } catch (err) {
     res.status(500).send('DATABASE ERROR: ' + err.message)
   }
