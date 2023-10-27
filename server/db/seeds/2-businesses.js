c9e1966df69cd5bcf65bf65067462a9d387796ca
Pathik Modi <modipathik95@gmail.com>
2023-10-27 15:24:39 +1300

    refactor homepage to show all business

--- server/db/seeds/2-businesses.js
+++ server/db/seeds/2-businesses.js
@@ -1,32 +1,32 @@
 export async function seed(knex) {
   await knex('businesses').insert([
     {
-      name: 'Doctors',
+      name: 'Cute Doctors',
       email: 'doctors@gmail.com',
       avg_appointment_time_mins: 90,
       css_theme: 'doctorTheme',
-      logo: 'doctorsEmoji',
+      logo: 'hospital.png',
     },
     {
-      name: 'Babers',
+      name: 'BarberShop',
       email: 'barbers@gmail.com',
       avg_appointment_time_mins: 60,
       css_theme: 'barberTheme',
-      logo: 'barbersEmoji',
+      logo: 'barber.png',
     },
     {
       name: 'Mechanics',
       email: 'mechanics@gmail.com',
       avg_appointment_time_mins: 30,
       css_theme: 'mechanicTheme',
-      logo: 'mechanicEmoji',
+      logo: 'mechanic.png',
     },
     {
       name: 'Vampire Phlebs',
       email: 'vampire@gmail.com',
       avg_appointment_time_mins: 15,
       css_theme: 'vampireTheme',
-      logo: 'vampireEmoji',
+      logo: 'blood-test.png',
     },
   ])
 }
